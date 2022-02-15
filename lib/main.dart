28c54827ea6b4e7c62a4b9b98754bd5748a467d1
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-02-15 19:03:31 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,63 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
   final questions = const [
     {
-      'questionText': 'What\'s your favorite anime?',
-      'answers': ['Demon Slayer', 'One Punch Man', 'Attack on Titan'],
+      'questionText': 'Who is the main character of the anime Pokémon?',
+      'answers': [
+        {'text': 'Misty', 'score': 0},
+        {'text': 'Ash Ketchum', 'score': 1},
+        {'text': 'Brock', 'score': 0},
+        {'text': 'Professor Samuel Oak', 'score': 0},
+      ],
     },
     {
-      'questionText': 'How many seasons have you watched?',
-      'answers': ['1', '2', '3', '4'],
+      'questionText': 'In \'Nurato\', the main character, Naruto Uzumaki, is a host for the powerful Nine-Tales. What creature is the Nine-Tails?',
+      'answers': [
+        {'text': 'Fox', 'score': 1},
+        {'text': 'Wolf', 'score': 0},
+        {'text': 'Bear', 'score': 0},
+        {'text': 'Cat', 'score': 0},
+      ],
     },
     {
-      'questionText': 'How many stars will you rate it?',
-      'answers': ['1', '2', '3', '4', '5'],
+      'questionText': 'In \'One Piece\', Monkey D. Luffy originally sets out with the Straw Hat Pirates to become the pirate king on which ship?',
+      'answers': [
+        {'text': 'Jolly Roger', 'score': 0},
+        {'text': 'Ever Darker', 'score': 0},
+        {'text': 'Going Merry', 'score': 1},
+        {'text': 'Thousand Sunny', 'score': 0},
+      ],
+    },
+    {
+      'questionText': 'Which planet is also known as the Dragon World in \'Dragon Ball\'?',
+      'answers': [
+        {'text': 'Mars', 'score': 0},
+        {'text': 'Earth', 'score': 1},
+        {'text': 'Jupiter', 'score': 0},
+        {'text': 'Neptune', 'score': 0},
+      ],
+    },
+    {
+      'questionText': 'In the anime Death Note, who was the first successor of L in the Kira investigation?',
+      'answers': [
+        {'text': 'Light Yagami', 'score': 1},
+        {'text': 'Near', 'score': 0},
+        {'text': 'Mello', 'score': 0},
+        {'text': 'Teru Mikami', 'score': 0},
+      ],
     },
   ];
   var questionIndex = 0;
+  var totalScore = 0;
+  
+  void resetQuiz() {
+    setState(() {
+      questionIndex = 0;
+      totalScore = 0;
+    });
+  }
 
-  void answerQuestion() {
+  void answerQuestion(int score) {
+    totalScore += score;
     setState(() {
       questionIndex += 1;
     });
@@ -48,7 +90,7 @@ class MyAppState extends State<MyApp> {
                 questionIndex: questionIndex,
                 questions: questions,
               )
-            : Result(),
+            : Result(totalScore,resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const Result({Key? key}) : super(key: key);
+  final int resultScore;
+  final VoidCallback reset;
+
+  Result(this.resultScore, this.reset);
+
+  String get resultPhrase {
+    var resultText = 'Casual';
+    if (resultScore >= 4) {
+      resultText = 'Elitist';
+    } else if(resultScore >=1) {
+      resultText = 'Weeaboo';
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'Thanks for your feedback!',
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          ElevatedButton(
+            onPressed: reset,
+            child: Text('Restart Quiz'),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+              foregroundColor: MaterialStateProperty.all(Colors.white70),
+            ),
+          ),
+        ],
       ),
     );
   }
